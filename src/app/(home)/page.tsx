405b691f6d44dfa3fa55203445c14b751e9b885b
italopereirajs <italo.pereira@verndale.com>
2025-03-12 21:23:41 -0300

    init: project scaffolding for Brand Zone reference management system

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Mulish } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const mulish = Mulish({
+  variable: "--font-mulish",
   subsets: ["latin"],
 });
 
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mulish.variable} antialiased`}
       >
         {children}
       </body>
