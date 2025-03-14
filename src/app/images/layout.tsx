bcf8a0df3c53625da0d8ffbe3d060094fb6f1675
italopereirajs <italo.pereira@verndale.com>
2025-03-14 19:37:24 -0300

    feat: add v1 of images page

---
+++ src/app/images/layout.tsx
@@ -0,0 +1,10 @@
+const ImagesLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <p>Navbar</p>
+      {children}
+    </div>
+  );
+};
+
+export default ImagesLayout;
